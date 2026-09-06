fb105ed4127fcea03bff7f3dfd97bda7d5986c94
SiliconPulse50 <3755622927@qq.com>
2026-09-06 21:45:57 +0800

    员工分页查询代码设计

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,8 +3,10 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
@@ -13,10 +15,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -81,6 +80,10 @@ public class EmployeeController {
         return Result.success();
     }
 
+
+
+
+
     @PostMapping
     @ApiOperation("新增员工")
       public Result save(@RequestBody EmployeeDTO employeeDTO){
@@ -88,6 +91,16 @@ public class EmployeeController {
         System.out .println("当前线程的id"+Thread.currentThread());
         employeeService.save(employeeDTO);
       return Result.success();
+    }
+
+    //参数是query格式的，不是json
+    @GetMapping("/page")
+    @ApiOperation("员工分页")
+
+    public Result<PageResult>page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("员工分页查询，参数为：{}",employeePageQueryDTO);
+        PageResult pageResult=employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
