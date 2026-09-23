ccdcbb5f3296bc23f0f8d1635259a3642678793a
SiliconPulse50 <3755622927@qq.com>
2026-09-23 23:29:10 +0800

    套餐管理:起售停售套餐的代码

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -19,4 +19,11 @@ public interface SetmealService {
     SetmealVO getByIdWithDish(Long id);
 
     void update(SetmealDTO setmealDTO);
+
+    /**
+     * 套餐起售，停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
